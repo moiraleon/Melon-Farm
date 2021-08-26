0a6b7f47074426efd3c5f17b1524d604d640d45a
Moira Leon <moira.leon2@gmail.com>
2021-08-26 16:31:07 -0600

    adding melon types

--- Main.java
+++ Main.java
@@ -4,4 +4,42 @@ public class Main {
 		System.out.println("Instantiate objects here!");
 	}
 
+	public class MelonType{
+
+		MelonType musk = new MelonType(
+				"musk",
+				"Muskmelon",
+				1998,
+				"green",
+				True,
+				True
+		);
+		MelonType casaba = new MelonType(
+				"cas",
+				"Casaba",
+				2003,
+				"orange",
+				False,
+				False
+		);
+		MelonType crenshaw = new MelonType(
+				"cren",
+				"Crenshaw",
+				1996,
+				"green",
+				False,
+				False
+		);
+		MelonType yellowWatermelon = new MelonType(
+				"yw",
+				"Yellow Watermelon",
+				2013,
+				"yellow",
+				False,
+				True
+		);
+
+
+	}
+
 }
